2b55ec0432d04b6b60ca56eaad912d425954de4a
ashwinexe <kumarashwin2603@gmail.com>
2023-04-02 23:22:18 +0530

    updated information on readme

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def inception_no_gen(image):
 def plot_pred_final(test_imgs):
   """
   dis takes in {1:prob(1),2:prob(2)}
-  and plots a SUPER NORMIE PLOT to make it easier for SRM FACULTY(or they might flip out like the bunch of idiots they are)
+
   """
   #test_imgs = glob(image_path_custom + '/*/*.jpeg')
   fig = make_subplots(rows = 2, cols = 2)
@@ -68,36 +68,28 @@ def plot_pred_final(test_imgs):
 #------streamlit starts here----------------
 
 model_saved = tensorflow.keras.models.load_model("inception_food_rec_50epochs.h5")
-target_dict = {0:"Bread",1:"Dairy_product",2:"Dessert",3:"Egg",4:"Fried_food",
+target_dict = {0:"Bread",1:"Dairy_product, 200 calorie",2:"Dessert",3:"Egg",4:"Fried_food",
                  5:"Meat",6:"Noodles/Pasta",7:"Rice",8:"Seafood",9:"Soup",10:"veggies/Fruit"}
 ss.set_page_config(page_title = "Food Recognition using Inception V3", layout = "wide")
 ss.title("An AI Based Neutirend Tracking and Analysis ")
 
 ss.markdown(
 '''
-This is the final year group project for IV-A Batch 13\n
+This is the final year group project for ECM IV-A Batch 13\n
 ''')
 
 # ss.image("f1.jpg")
 ss.markdown(
 '''
-- The paper for Inception can be found [here](https://arxiv.org/abs/1512.00567v3)\n
-
-- The paper implementation using pytorch can be found [here](https://github.com/pytorch/vision/blob/6db1569c89094cf23f3bc41f79275c45e9fcb3f3/torchvision/models/inception.py#L64)
-
-- Inception-v3 is a convolutional neural network architecture from the Inception family that makes several improvements including using 
-  - Label Smoothing,
-  - Factorized 7 x 7 convolutions,\n 
-  and the use of an auxiliary classifer to propagate label information lower down the network (along with the use of batch normalization for layers in the sidehead).
-- Training on 16,600 images yielded 90% accuracy on train and 76% accuracy on validation. over 50 epochs!
+- Training on 16,600 images yielded 90% accuracy on train and 76% accuracy on validation. over 50 epochs.
 - This model is saved and used later
 '''
 )
 ss.markdown(
 '''
-### Model Architecture
+### Block Diagram
 ''')
-ss.image("inception_2.png")
+ss.image("block_diagram.png")
 
 ss.markdown('### Dataset Details and Classes')
 ss.markdown('Data consists of 1.1GB of 16,600 images of different categories of food.')
@@ -127,6 +119,10 @@ if image_path:
   image = Image.open(image_path)
   preds = plot_pred_final(image)
   ss.plotly_chart(preds)
+
+  # cal = preds
+  # ss.write("Nutrient Intake:-")
+  # ss.warning('**'+cal+'(Intake)**')
   
 
 ss.markdown('''
@@ -135,5 +131,3 @@ This project is made by
 - Sanjana Reddy 19311A1908
 - Raveena Ganji 19311A1958
 ''')
-
-
